ff6190a0a942bcf94f1a1013fde94b53f00e4507
odoo <grmdenis@gmail.com>
2019-04-22 09:52:10 -0400

    Add files via upload

---
+++ innscience_price_hide/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Public price Hide",
+
+    'summary': 'In Website shop hide public price',
+
+    'description': """Hide public price in website shop
+""",
+
+    'author': "Innscience.ca",
+    'website': "http://innscience.ca",
+    'category': 'Website',
+    'version': '12.0.0.1',
+    'installable': True,
+    'license': 'OPL-1',
+	'support':'grmdenis@gmail.com',
+    'iap':'M1806127894570',
+    'price':'10',
+    'currency':'USD',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml',
+    ],
+}
